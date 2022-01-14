d5e07a0f7f5597e05eea3051f11d14363b2b0b97
xenonminer <46347858+xenonminer@users.noreply.github.com>
2022-01-14 02:08:33 -0500

    Add files via upload

---
+++ Files/crypto/Caesar's Palace/main.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+#flag = open("flag.txt", "r").readline()
+dictionary = open("dictionary.txt", "r").read().splitlines()
+
+def caesar(txt, s):
+    result = ""
+    for i in range(len(txt)):  
+        char = txt[i]  
+        result += chr((ord(char) + int(s) - 96) % 26 + 97)  
+    return result
+
+print("\nWelcome to Caesar's Palace!")
+
+for n in range(200):
+	word = random.choice(dictionary).lower()
+	caesared_word = caesar(word, random.choice(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']))
+	print(f" Encrypted word: {caesared_word}")
+	user_input = input("Decrypted word: ")
+	if user_input == word:
+		print("Good")
+	else:
+		print("That is not the right decrypted word!")
+		exit()
+print(f"Nice job, you are the caesar master: {flag}")
