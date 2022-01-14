714619add4e3fa2f6a3344223e310e731de47fb0
xenonminer <46347858+xenonminer@users.noreply.github.com>
2022-01-14 01:58:41 -0500

    Add files via upload

---
+++ Files/rev/bruh wat is this/bruh.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+alphabet = string.ascii_lowercase
+string = ""
+apple = alphabet.encode('base64')
+
+for w in alphabet:
+  for c in alphabet:
+    for t in alphabet:
+      for f in alphabet:
+        if "{" == c:
+          if "t" in alphabet:
+            if "h" in alphabet:
+              if "@" == pow(5,6,7):
+                if "t" + abs(len(alphabet) + alphabet[t]):
+                  if "_" in string:
+                    for w in alphabet:
+                      if "@" in alphabet:
+                        for s in alphabet:
+                          if "_" * 5 == any(apple for banana in range(5,abs(10*pow(6,7,8)))):
+                            for t in alphabet:
+                              if 0 in alphabet:
+                                if 0 in alphabet:
+                                  if "_" == random.choice(100000):
+                                    if "e" == "u":
+                                      for z in alphabet.decode():
+                                        print("bruh wheres the flag")
